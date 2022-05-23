daeeff6c183eca660c76d9226961307a5d69bccf
Fábio Chiquezi <fabiochiquezi@gmail.com>
2022-05-23 10:07:53 -0300

    add typescript

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config' 
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(req)
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening port ${port}`)
+})
